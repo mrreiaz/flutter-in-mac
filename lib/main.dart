0cab698b280fd7c1092c053c97a09e0e2cfc8957
mrreiaz <reiazbubt@gmail.com>
2023-10-03 21:24:13 +0900

    Expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,39 +12,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(108.0),
-            child: Container(
-              child: Center(
-                child: Text(
-                  'Reiaz',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.yellow,
                 ),
               ),
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                shape: BoxShape.circle,
-                //borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade400,
-                    offset: const Offset(4.0, 4.0),
-                    blurRadius: 5.0,
-                    spreadRadius: 1.0,
-                  ),
-                  const BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-4.0, -4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0,
-                  ),
-                ],
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.pink,
+                ),
               ),
-            ),
+              Expanded(
+                child: Container(
+                  color: Colors.black87,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                ),
+              ),
+            ],
           ),
         ),
       ),
