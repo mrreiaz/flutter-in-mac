ebd17013050dbcc5145809c08a1488919ce73192
mrreiaz <reiazbubt@gmail.com>
2023-10-08 22:55:31 +0900

    animated container

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,43 +1,45 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  double boxHight = 200.0;
+  double boxWidth = 200.0;
+
+  var boxColor = Colors.pink;
+
+  void _expandBox() {
+    setState(() {
+      boxHight = 300.0;
+      boxWidth = 300.0;
+    });
+  }
+
+  void _changeBoxColor() {
+    setState(() {
+      boxColor = Colors.deepPurple;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(108.0),
-          child: Container(
-            child: Center(
-              child: Text(
-                'Reiaz',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            height: 200,
-            width: 200,
+      backgroundColor: Colors.pink[100],
+      body: GestureDetector(
+        onTap: _changeBoxColor,
+        child: Center(
+          child: AnimatedContainer(
+            duration: Duration(seconds: 2),
+            height: boxHight,
+            width: boxWidth,
             decoration: BoxDecoration(
-              color: Colors.grey[300],
-              shape: BoxShape.circle,
-              //borderRadius: BorderRadius.circular(30),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.shade400,
-                  offset: const Offset(4.0, 4.0),
-                  blurRadius: 5.0,
-                  spreadRadius: 1.0,
-                ),
-                const BoxShadow(
-                  color: Colors.white,
-                  offset: Offset(-4.0, -4.0),
-                  blurRadius: 15.0,
-                  spreadRadius: 1.0,
-                ),
-              ],
+              borderRadius: BorderRadius.circular(10),
+              color: boxColor,
             ),
           ),
         ),
