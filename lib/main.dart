b594dc90c8ba7b5e488221c63b1d0f48fe304aa6
mrreiaz <reiazbubt@gmail.com>
2023-10-08 18:12:05 +0900

    clipRRect

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Container(
+              height: 300,
+              width: 300,
+              color: Colors.orange,
+            ),
+          ),
+          // child: Container(
+          //   height: 300,
+          //   width: 300,
+          //   decoration: BoxDecoration(
+          //     borderRadius: BorderRadius.circular(60.0),
+          //     color: Colors.blue,
+          //   ),
+          // ),
+        ),
+      ),
+    );
   }
 }
