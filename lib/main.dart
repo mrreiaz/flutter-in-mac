c5d91579c2580ec17cacf066a09be518c908b7b9
mrreiaz <reiazbubt@gmail.com>
2023-10-08 22:06:19 +0900

    play with sliver

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,70 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(108.0),
-            child: Container(
-              child: Center(
-                child: Text(
-                  'Reiaz',
-                  style: TextStyle(
-                    fontSize: 30,
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: Icon(Icons.menu),
+              //title: Text('MrReiaz!'),
+              expandedHeight: 180,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  color: Colors.orange,
+                ),
+                title: Text('MrReiaz'),
+              ),
+              floating: true,
+              pinned: true,
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    color: Colors.cyan,
+                    height: 300,
                   ),
                 ),
               ),
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                shape: BoxShape.circle,
-                //borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade400,
-                    offset: const Offset(4.0, 4.0),
-                    blurRadius: 5.0,
-                    spreadRadius: 1.0,
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    color: Colors.cyan[200],
+                    height: 300,
                   ),
-                  const BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-4.0, -4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0,
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    color: Colors.cyan[400],
+                    height: 300,
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    color: Colors.cyan[900],
+                    height: 300,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
