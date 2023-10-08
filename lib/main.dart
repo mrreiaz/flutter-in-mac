48fcb96999ed7dc8ca5d6ee2eafa8b89fd834818
mrreiaz <reiazbubt@gmail.com>
2023-10-08 18:25:51 +0900

    incrage and discharge a number

--- lib/main.dart
+++ lib/main.dart
@@ -4,46 +4,80 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int numberOfTimesTap = 0;
+  void _incrageANumber() {
+    setState(() {
+      numberOfTimesTap++;
+    });
+  }
+
+  void _dicrageANumber() {
+    setState(() {
+      if (numberOfTimesTap <= 0) {
+        numberOfTimesTap = 0;
+      } else {
+        numberOfTimesTap--;
+      }
+    });
+  }
+
+  void _makeANumberZero() {
+    setState(() {
+      numberOfTimesTap = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(108.0),
-            child: Container(
-              child: Center(
-                child: Text(
-                  'Reiaz',
-                  style: TextStyle(
-                    fontSize: 30,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Tap $numberOfTimesTap Times!'),
+                Container(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: _incrageANumber,
+                        child: Container(
+                          padding: EdgeInsets.all(40),
+                          color: Colors.orange,
+                          child: Text('+'),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: _dicrageANumber,
+                        child: Container(
+                          padding: EdgeInsets.all(40),
+                          color: Colors.orange,
+                          child: Text('-'),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: _makeANumberZero,
+                        child: Container(
+                          padding: EdgeInsets.all(40),
+                          color: Colors.orange,
+                          child: Text('Make Zero'),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                shape: BoxShape.circle,
-                //borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade400,
-                    offset: const Offset(4.0, 4.0),
-                    blurRadius: 5.0,
-                    spreadRadius: 1.0,
-                  ),
-                  const BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-4.0, -4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0,
-                  ),
-                ],
-              ),
+              ],
             ),
           ),
         ),
