dc79a2d94be7858b69fa49476299300694f66d46
mrreiaz <reiazbubt@gmail.com>
2023-10-08 20:30:03 +0900

    drawe and fix problem drawe link is not working inside materialApp

---
+++ lib/fist_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FistPage extends StatelessWidget {
+  const FistPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text('sdds')),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutterinmac/fist_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void _navigateToNextScreen() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => FistPage(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(108.0),
-            child: Container(
-              child: Center(
-                child: Text(
-                  'Reiaz',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-              ),
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                shape: BoxShape.circle,
-                //borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade400,
-                    offset: const Offset(4.0, 4.0),
-                    blurRadius: 5.0,
-                    spreadRadius: 1.0,
-                  ),
-                  const BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-4.0, -4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0,
-                  ),
-                ],
-              ),
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Center(child: Text("H E L L O")),
             ),
-          ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: _navigateToNextScreen,
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      endDrawer: Drawer(),
+      body: SafeArea(
+        child: Center(
+          child: Text('Hello!'),
         ),
       ),
     );
